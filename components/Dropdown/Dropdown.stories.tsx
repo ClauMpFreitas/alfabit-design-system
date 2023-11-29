cab964a93011c1c6cb2e5edd1c1e7acc1bcaab75
ClauMpFreitas <claudinhampfreitas@gmail.com>
2023-11-29 11:29:40 -0300

    Primeiro commit

---
+++ components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Dropdown, { type DropdownProps } from "./Dropdown";
+
+const meta: Meta<DropdownProps> = {
+  title: "Desing System/Molecules/Dropdown",
+  component: Dropdown,
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <div style={{ paddingBottom: "15em", paddingTop: "3em" }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<DropdownProps>;
+
+export const Primary: Story = {
+  args: {
+    list: [],
+  },
+};
+
+export const Common: Story = {
+  args: {
+    list: [
+      "Durward Reynolds",
+      "Kenton Towne",
+      "Therese Wunsch",
+      "Benedict Kessler",
+      "Katelyn Rohan",
+    ],
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Modal from "@/components/Modal/Modal";
+import Text from "@/components/Text/Text";
+import React from "react";
+
+const App = () => {
+  return <div>Olá mundo</div>;
+};
+
+export default App;
